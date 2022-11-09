0d6a678ae7ebd80b329de9fbeeec8fb235d6b67d
kusuma-rakesh <116748146+kusuma-rakesh@users.noreply.github.com>
2022-11-09 20:08:59 +0530

    Reorganized the fruityvice code
    Reorganized the fruityvice code to add try..except:
    try:
      fruit_choice= streamlit.text_input('What fruit information would you like to know?')
      if not fruit_choice:
        streamlit.error("Please select a fruit to get information")
      else:
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
        #streamlit.text(fruityvice_response)
        fruitevice_normalize = pandas.json_normalize(fruityvice_response.json()) 
        streamlit.dataframe(fruitevice_normalize)
     except URLError as e:
        streamlit.error()

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,19 +34,17 @@ streamlit.dataframe(fruites_to_show)
 streamlit.header("Fruitvice Fruit Advice!")
 
 #Adding search text bax and passing it as param to api call
-fruit_choice= streamlit.text_input('What fruit information would you like to know?','kiwi')
-streamlit.write('The user entered',fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-streamlit.text(fruityvice_response)
-
-#this below command help us show the response json on onscreen
-#streamlit.text(fruityvice_response.json())
-
-#Normalize the json
-fruitevice_normalize = pandas.json_normalize(fruityvice_response.json())
-
-#show output on screen in a grid
-streamlit.dataframe(fruitevice_normalize)
+try:
+  fruit_choice= streamlit.text_input('What fruit information would you like to know?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response)
+    fruitevice_normalize = pandas.json_normalize(fruityvice_response.json()) 
+    streamlit.dataframe(fruitevice_normalize)
+ except URLError as e:
+    streamlit.error()
 
 #to correct the records which are inserting into snoflake-- duplicate records like from streamlit in fruitlist table, first stop streamlit here
 streamlit.stop()
