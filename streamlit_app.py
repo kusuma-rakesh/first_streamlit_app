185df1c78dbe43df2bf16e0270ec412f508b1ef6
kusuma-rakesh <116748146+kusuma-rakesh@users.noreply.github.com>
2022-11-09 19:56:40 +0530

    corrected the typo- urllib
    corrected the typo- urllib

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urlib.error import URLError
+from urllib.error import URLError
 
 #import streamlit
 streamlit.title('My Parent New Healthy Diner')
